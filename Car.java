d302809b08734d89a922b74f89c12b376f0ab62e
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2026-01-27 17:31:43 +0100

    refactor: add Car as abstract superclass

---
+++ Car.java
@@ -0,0 +1,65 @@
+import java.awt.Color;
+
+public abstract class Car {
+    private int nrDoors;
+    protected double enginePower;
+    private double currentSpeed;
+    private Color color;
+    public String modelName;
+
+    public Car(int nrDoors, Color color, double enginePower, String modelName) {
+        this.nrDoors = nrDoors;
+        this.color = color;
+        this.enginePower = enginePower;
+        this.modelName = modelName;
+        stopEngine();
+    }
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color clr) {
+        color = clr;
+    }
+
+    public void startEngine() {
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine() {
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    public void incrementSpeed(double amount) {
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
+    }
+
+    public void decrementSpeed(double amount) {
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
+    }
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount) {
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount) {
+        decrementSpeed(amount);
+    }
+}
