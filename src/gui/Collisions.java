dcf19fba2bcc281f89f4007028c012ee73d9e771
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2026-02-19 21:37:25 +0100

    chore: remove unused

--- src/gui/Collisions.java
+++ src/gui/Collisions.java
@@ -6,33 +6,6 @@ import java.awt.geom.Rectangle2D;
 import javax.swing.JPanel;
 
 public class Collisions {
-    public static boolean valueInRange(double value, double min, double max) {
-        return (min < value && value < max);
-    }
-
-    public static boolean rectangleOverlaps(Rectangle2D.Double rectA, Rectangle2D.Double rectB) {
-        return rectangleOverlaps(
-                rectA.x, rectA.y, rectA.width, rectA.height,
-                rectB.x, rectB.y, rectB.width, rectB.height);
-    }
-
-    public static boolean rectangleOverlaps(
-            double xA, double yA, double widthA, double heightA,
-            double xB, double yB, double widthB, double heightB) {
-
-        // x: [workshop.x - car.width, workshop.x + workshop.width]
-        // y: [workshop.y - car.height, workshop.y + workshop.height]
-
-        double xmin = xB - widthA;
-        double xmax = xB + widthB;
-
-        double ymin = yB - heightA;
-        double ymax = yB + heightB;
-
-        return valueInRange(xA, xmin, xmax) && valueInRange(yA, ymin, ymax);
-
-    }
-
     public static boolean collidesWithEdges(JPanel drawPanel, Rectangle2D.Double bounds) {
         Dimension frameSize = drawPanel.getSize();
 
